e1291bff40b2dd1d580db0cf0b044b60c97bfbb7
Asheesh01 <ka301423@gmail.com>
2026-06-19 16:47:31 +0530

    Initial commit

---
+++ 03-site-banner/src/index.js
@@ -0,0 +1,37 @@
+import express from "express"
+import Redis from "ioredis"
+const app=express()
+app.use(express.json());
+
+const redis=new Redis(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
+
+redis.on("connect",()=>{
+    console.log("Redis connected")
+})
+redis.on("error",()=>{
+    console.log("Redis error",err.message)
+})
+const BANNER_KEY="app:banner";
+
+app.post("/banner",async(req,res)=>{
+    await redis.set(BANNER_KEY,req.body.message || "welcome to chai aur redis");
+    res.json({success:true})
+})
+app.get("/banner",async(req,res)=>{
+   const message= await redis.get(BANNER_KEY);
+    res.json({message})
+})
+
+app.delete("/banner",async(req,res)=>{
+   const message= await redis.del(BANNER_KEY);
+    res.json({deleted:message})
+})
+
+app.get("/banner/exists",async(req,res)=>{
+    const exists=await redis.exists(BANNER_KEY)
+    res.json({exists: Boolean(exists)});
+})
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
